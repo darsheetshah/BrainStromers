28c521dd8eb44289a178e1a653015cb6874fac40
Darsheet Shah <darsheets@yahoo.com>
2012-04-09 14:11:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrainStromers::Application.config.secret_token = '923fe11ea7e438651e62658bc78959755172e688f8d0d4d45811e0b620340d7068d4475d9f64ee77d75961259dc9102b2c031701a3f65c18f97eb552cc64b7ef'
